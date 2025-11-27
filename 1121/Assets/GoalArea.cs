9258e10b5df8a22c545f6d3dd4833baf60f228f2
조현창 <109057035+chc020604@users.noreply.github.com>
2025-11-28 03:49:35 +0900

    Add files via upload

---
+++ 1121/Assets/GoalArea.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GoalArea : MonoBehaviour
+{
+    public static bool goal;
+
+    void Start()
+    {
+        goal = false;
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            goal = true;
+        }
+    }
+}
